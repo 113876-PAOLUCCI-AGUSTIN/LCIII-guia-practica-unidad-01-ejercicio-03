216d5279f55a82e2f7eab3b7f07bc8ca190002a3
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-20 21:45:41 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * LC_III-guía-práctica-unidad-01-ejercicio-03
+ * Hacer un programa que ingrese los datos de una factura en la cual haya tres
+ * artículos vendidos. De cada artículo ingresar el precio unitario y la cantidad.
+ * Finalmente, imprimir el total de la factura.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
